ba39e04fffdb2a7765eda3d5a70aabcf52df6f59
Gabriela <krustagabriela@gmail.com>
2023-05-04 13:23:27 +0200

    hex done

--- ft_hexadec.c
+++ ft_hexadec.c
@@ -6,27 +6,34 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:15:44 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/05/03 16:25:03 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/05/04 13:23:26 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_hexadec(int n)
+#include "printf.h"
+
+int	ft_hexadec(unsigned long  n, char c)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
 	if (n < 0)
 		return (-1);
-	if 
 	if (n < 16)
 	{
-		ft_hexadec(n + '0');
+		if (c == 'x')
+			ft_putchar("0123456789abcdef"[n % 16]);
+		else
+			ft_putchar("0123456789ABCDEF"[n % 16]);
 		i++;
 	}
 	else
 	{
-		ft_hexadec(n / 16);
-		i += ft_putchar(n % 10 + '0');
+		ft_hexadec(n / 16, c);
+		if (c == 'x')
+			i += ft_putchar("0123456789abcdef"[n % 16]);
+		else
+			i += ft_putchar("0123456789ABCDEF"[n % 16]);
 	}
 	return (i);
 }
--- ft_void_ptr.c
+++ ft_void_ptr.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_void_ptr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/03 10:34:21 by gkrusta           #+#    #+#             */
+/*   Created: 2023/05/04 12:21:26 by gkrusta           #+#    #+#             */
 /*   Updated: 2023/05/04 13:23:26 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-#include <stdio.h>
-int	main(void)
+
+int	ft_void_ptr(unsigned long ptr)
 {
-	int	c = 1234;
-	int	*ptr = &c;
-/* 	char	str[] = "hola123"; */
-/* 	printf("%p", (void *)ptr); */
-/* 	ft_decimal(1234); */
-/* 	ft_hexadec(0, 'x'); */
-	ft_void_ptr();
-	return (0);
+	int	i;
+
+	i = 0;
+	i += ft_putstr("0x");
+	i += ft_hexadec(n);
+	return (i);
 }
