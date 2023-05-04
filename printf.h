57fc305dc8afe8b1fd0d537dd901dd107b429538
Gabriela <krustagabriela@gmail.com>
2023-05-04 15:10:21 +0200

    added compare

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 16:19:25 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/05/04 13:19:02 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/05/04 15:10:20 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 
 /* library used */
 # include <unistd.h>
+# include <stdarg.h>
 
 /* functions used */
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_decimal(int n);
+int	ft_us_decimal(unsigned long n);
 int	ft_hexadec(unsigned long n, char c);
-int	ft_void_ptr(unsigned long n n);
+int	ft_void_ptr(unsigned long n);
 
 #endif
